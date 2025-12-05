7daa7e7d583005d30ce2a2f73db218b7e9bd4319
aljazbrodar. <aljaz.brodar@gmail.com>
2025-12-05 14:10:31 +0100

    Added proto files.

---
+++ cmd/server/main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+    "log"
+    "net"
+
+    "google.golang.org/grpc"
+    pb "github.com/ab76015/razpravljalnica/api/pb"
+)
+
+type server struct {
+    pb.UnimplementedRazpravljalnicaServer
+}
+
+func main() {
+    lis, err := net.Listen("tcp", ":50051")
+    if err != nil {
+        log.Fatalf("listen error: %v", err)
+    }
+
+    s := grpc.NewServer()
+    pb.RegisterRazpravljalnicaServer(s, &server{})
+
+    log.Println("Server listening on :50051")
+    if err := s.Serve(lis); err != nil {
+        log.Fatalf("server error: %v", err)
+    }
+}
+
