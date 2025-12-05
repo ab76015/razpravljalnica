e02288052feea1e8c895a4401cbb19578d99785a
aljazbrodar. <aljaz.brodar@gmail.com>
2025-12-05 14:51:06 +0100

    Minimalni server, sledi client...

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -13,15 +13,18 @@ type server struct {
 }
 
 func main() {
+    // Ustvarimo TCP server na vratih 50051
     lis, err := net.Listen("tcp", ":50051")
     if err != nil {
         log.Fatalf("listen error: %v", err)
     }
-
+    // Ustvarimo gRPC server
     s := grpc.NewServer()
+    // Registriramo implementacijo servisa (MessageBoard) pri gRPC strežniku s
     pb.RegisterMessageBoardServer(s, &server{})
 
     log.Println("Server listening on :50051")
+    // Začni sprejemati dohodne omrežne povezave na lis (listenerju) in servirati gRPC klice
     if err := s.Serve(lis); err != nil {
         log.Fatalf("server error: %v", err)
     }
