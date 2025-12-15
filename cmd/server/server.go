f0cd7f81e8cc64e062d78bcbac43022c8c1d7414
ReiM04 <proma.reimarc@gmail.com>
2025-12-15 23:00:20 +0100

    Like Update in Delete dodani v server

--- cmd/server/server.go
+++ cmd/server/server.go
@@ -39,3 +39,27 @@ func (s *Server) PostMessage(ctx context.Context, in *pb.PostMessageRequest) (*p
     return &pb.Message{Id : message.ID, TopicId: message.TopicID, UserId: message.UserID, Text: message.Text, CreatedAt: message.CreatedAt, Likes: message.Likes}, nil
 }
 
+func (s *Server) UpdateMessage(ctx context.Context, in *pb.UpdateMessageRequest) (*pb.Message, error) {
+    message, err := s.storage.UpdateMessage(in.TopicId, in.UserId, in.MessageId, in.Text)
+    if err != nil {
+        return nil, err
+    }
+    return &pb.Message{Id : message.ID, TopicId: message.TopicID, UserId: message.UserID, Text: message.Text, CreatedAt: message.CreatedAt, Likes: message.Likes}, nil
+}
+
+func (s *Server) DeleteMessage(ctx context.Context, in *pb.DeleteMessageRequest) (*emptypb.Empty, error) {
+    err := s.storage.DeleteMessage(in.TopicId, in.UserId, in.MessageId)
+    if err != nil {
+        return nil, err
+    }
+    return &emptypb.Empty{}, nil
+}
+
+func (s *Server) LikeMessage(ctx context.Context, in *pb.LikeMessageRequest) (*pb.Message, error) {
+    message, err := s.storage.LikeMessage(in.TopicId, in.MessageId, in.UserId)
+    if err != nil {
+        return nil, err
+    }
+    return &pb.Message{Id : message.ID, TopicId: message.TopicID, UserId: message.UserID, Text: message.Text, CreatedAt: message.CreatedAt, Likes: message.Likes}, nil
+}
+
