69e21f7e6c6dfe80b366eab954b5bf1c35a3a05f
aljazbrodar. <aljaz.brodar@gmail.com>
2025-12-21 14:04:08 +0100

    node fix

--- internal/replication/node.go
+++ internal/replication/node.go
@@ -5,13 +5,18 @@ import (
     "github.com/ab76015/razpravljalnica/internal/storage"
 )
 
+const (
+    Head   = 0
+    Middle = 1
+    Tail   = 2
+)
+
 type Node struct {
     Info *pb.NodeInfo
 
-    IsHead bool
-    IsTail bool
+    Role int // 0=head, 1=middle, 2=tail
 
     Store storage.Storage
-    Next  *Node // naslednji v verigi (nil če tail)
+    Next  pb.ReplicationClient
 }
 
