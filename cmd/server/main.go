a67c7c31d7aeab97293a33ddb7e1c402363a96b3
aljazbrodar. <aljaz.brodar@gmail.com>
2025-12-29 10:15:04 +0100

    Server se razdeli na 2 dela -> (messageboard, replikacija)

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -7,6 +7,7 @@ import (
     "google.golang.org/grpc"
     pb "github.com/ab76015/razpravljalnica/api/pb"
     "github.com/ab76015/razpravljalnica/internal/storage"
+    "github.com/ab76015/razpravljalnica/internal/replication"
 )
 
 func main() {
@@ -17,11 +18,16 @@ func main() {
 
     grpcServer := grpc.NewServer() //ustvari instanco grpc serverja
 
-    st := storage.NewMemStorage()
-    srv := NewServer(st)
+    storage := storage.NewMemStorage()
+    messageBoardSrv := NewMessageBoardServer(storage)
 
-    pb.RegisterMessageBoardServer(grpcServer, srv) //povezemo z naso implementacijo
-
+    nodeState := replication.NewNodeState()
+    replicationSrv := replication.NewDataNodeServer(nodeState)
+    
+    // povezemo z nasimi implementacijami
+    pb.RegisterMessageBoardServer(grpcServer, messageBoardSrv)
+    pb.RegisterDataNodeServer(grpcServer, replicationSrv)
+    
     log.Println("listening on :50051")
     grpcServer.Serve(lis) //server tu blokira do klica Stop() ali dokler proces ni killed
 }
