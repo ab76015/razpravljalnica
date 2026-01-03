47d245cf1f25d39c4758330e0e04b510157c51c8
ReiM04 <proma.reimarc@gmail.com>
2026-01-03 03:05:46 +0100

    Marshalling se za ostale metode in odstranitev par errorju

--- internal/replication/node.go
+++ internal/replication/node.go
@@ -1,218 +1,250 @@
 package replication
 
 import (
-    "log"
-    "context"
-    "sync"
-    "google.golang.org/grpc"
-    pb "github.com/ab76015/razpravljalnica/api/pb"
-    "google.golang.org/protobuf/types/known/emptypb"
+	"context"
+	"fmt"
+	"log"
+	"sync"
+
+	pb "github.com/ab76015/razpravljalnica/api/pb"
+	"google.golang.org/grpc"
+	"google.golang.org/protobuf/proto"
+	"google.golang.org/protobuf/types/known/emptypb"
 )
 
 // NodeState vsebuje lokalno informacijo o sosedih in verzijo verige
 type NodeState struct {
-    mu          sync.RWMutex
-    predecessor *pb.NodeInfo
-    successor   *pb.NodeInfo
-    self        *pb.NodeInfo
-    head        *pb.NodeInfo
-    tail        *pb.NodeInfo
-    version     uint64
+	mu          sync.RWMutex
+	predecessor *pb.NodeInfo
+	successor   *pb.NodeInfo
+	self        *pb.NodeInfo
+	head        *pb.NodeInfo
+	tail        *pb.NodeInfo
+	version     uint64
 }
 
 // NewNodeState ustvari zacetno prazno stanje
 func NewNodeState(self *pb.NodeInfo) *NodeState {
-    return &NodeState{self: self}
+	return &NodeState{self: self}
 }
 
 // UpdateConfig posodobi lokalno stanje verige za vozlisce
 func (ns *NodeState) UpdateConfig(cfg *pb.ChainConfig) {
-    ns.mu.Lock()
-    defer ns.mu.Unlock()
-    ns.predecessor = cfg.Predecessor
-    ns.successor = cfg.Successor
-    ns.head = cfg.Head
-    ns.tail = cfg.Tail
-    ns.version = cfg.Version
+	ns.mu.Lock()
+	defer ns.mu.Unlock()
+	ns.predecessor = cfg.Predecessor
+	ns.successor = cfg.Successor
+	ns.head = cfg.Head
+	ns.tail = cfg.Tail
+	ns.version = cfg.Version
 }
 
 // IsHead preveri ali je vozlisce glava
 func (ns *NodeState) IsHead() bool {
-    ns.mu.RLock()
-    defer ns.mu.RUnlock()
-    return ns.self != nil && ns.head != nil && ns.self.NodeId == ns.head.NodeId
+	ns.mu.RLock()
+	defer ns.mu.RUnlock()
+	return ns.self != nil && ns.head != nil && ns.self.NodeId == ns.head.NodeId
 }
 
 // IsTail preveri ali je vozlisce rep
 func (ns *NodeState) IsTail() bool {
-    ns.mu.RLock()
-    defer ns.mu.RUnlock()
-    return ns.self != nil && ns.tail != nil && ns.self.NodeId == ns.tail.NodeId
+	ns.mu.RLock()
+	defer ns.mu.RUnlock()
+	return ns.self != nil && ns.tail != nil && ns.self.NodeId == ns.tail.NodeId
 }
 
 // GetState vrne trenutni config state
 func (ns *NodeState) GetState() (pred, succ *pb.NodeInfo, version uint64) {
-    ns.mu.RLock()
-    defer ns.mu.RUnlock()
-    return ns.predecessor, ns.successor, ns.version
+	ns.mu.RLock()
+	defer ns.mu.RUnlock()
+	return ns.predecessor, ns.successor, ns.version
 }
 
 // Successor vrne naslednika vozlišča
 func (ns *NodeState) Successor() *pb.NodeInfo {
-    ns.mu.RLock()
-    defer ns.mu.RUnlock()
-    return ns.successor
+	ns.mu.RLock()
+	defer ns.mu.RUnlock()
+	return ns.successor
 }
 
 // Predecessor vrne predhodnika vozlišča
 func (ns *NodeState) Predecessor() *pb.NodeInfo {
-    ns.mu.RLock()
-    defer ns.mu.RUnlock()
-    return ns.predecessor
+	ns.mu.RLock()
+	defer ns.mu.RUnlock()
+	return ns.predecessor
 }
 
 // Version vrne verzijo vozlišča
 func (ns *NodeState) Version() uint64 {
-    ns.mu.RLock()
-    defer ns.mu.RUnlock()
-    return ns.version
+	ns.mu.RLock()
+	defer ns.mu.RUnlock()
+	return ns.version
 }
+
 // DataNodeServer implementira data node gRPC server interface (iz proto)
 type DataNodeServer struct {
-    pb.UnimplementedDataNodeServer
-    state *NodeState
+	pb.UnimplementedDataNodeServer
+	state   *NodeState
+	pending map[uint64]chan struct{}
+	mu      sync.Mutex
 }
 
 // NewDataNodeServer je konstruktor, ki sprejme NodeState in ustvari abstrakcijo streznika za verizno replikacijo
 func NewDataNodeServer(state *NodeState) *DataNodeServer {
-    return &DataNodeServer{state: state}
+	return &DataNodeServer{state: state}
 }
 
 // State vrne je getter za kazalec na stanje (NodeState) trenutnega DataNodeServer strežnika
 func (s *DataNodeServer) State() *NodeState {
-    s.state.mu.RLock()
-    defer s.state.mu.RUnlock()
-    return s.state
+	s.state.mu.RLock()
+	defer s.state.mu.RUnlock()
+	return s.state
 }
 
 // UpdateChainConfig RPC ki ga klice kontrolna ravnina ko zeli posodobiti stanje streznika na podatkovni ravnini
 func (s *DataNodeServer) UpdateChainConfig(ctx context.Context, cfg *pb.ChainConfig) (*emptypb.Empty, error) {
-    s.state.UpdateConfig(cfg)
-    log.Printf(
-    "[CHAIN-UPDATE] version=%d pred=(%v) succ=(%v)\n head=(%v) tail=(%v)\n",
-    cfg.Version,
-    cfg.Predecessor,
-    cfg.Successor,
-    cfg.Head,
-    cfg.Tail,
-    )
-    return &emptypb.Empty{}, nil
+	s.state.UpdateConfig(cfg)
+	log.Printf(
+		"[CHAIN-UPDATE] version=%d pred=(%v) succ=(%v)\n head=(%v) tail=(%v)\n",
+		cfg.Version,
+		cfg.Predecessor,
+		cfg.Successor,
+		cfg.Head,
+		cfg.Tail,
+	)
+	return &emptypb.Empty{}, nil
 }
 
 func (s *DataNodeServer) applyWrite(rw *pb.ReplicatedWrite) error {
-    switch rw.Op {
+	switch rw.Op {
 
-    case "PostMessage":
-        var req pb.PostMessageRequest
-        if err := proto.Unmarshal(rw.Payload, &req); err != nil {
-            return err
-        }
+	case "PostMessage":
+		var req pb.PostMessageRequest
+		if err := proto.Unmarshal(rw.Payload, &req); err != nil {
+			return err
+		}
 
-        return s.storage.PostMessage(&req)
+		return s.storage.PostMessage(&req)
 
-    case "CreateUser":
-        var req pb.CreateUserRequest
-        if err := proto.Unmarshal(rw.Payload, &req); err != nil {
-            return err
-        }
+	case "CreateUser":
+		var req pb.CreateUserRequest
+		if err := proto.Unmarshal(rw.Payload, &req); err != nil {
+			return err
+		}
 
-        return s.storage.CreateUser(&req)
-    // itd.. ...TODO
+		return s.storage.CreateUser(&req)
 
-    default:
-        return fmt.Errorf("unknown op %s", rw.Op)
-    }
+	case "CreateTopic":
+		var req pb.CreateTopicRequest
+		if err := proto.Unmarshal(rw.Payload, &req); err != nil {
+			return err
+		}
+		return s.storage.CreateTopic(&req)
+
+	case "UpdateMessage":
+		var req pb.UpdateMessageRequest
+		if err := proto.Unmarshal(rw.Payload, &req); err != nil {
+			return err
+		}
+		return s.storage.UpdateMessage(&req)
+
+	case "DeleteMessage":
+		var req pb.DeleteMessageRequest
+		if err := proto.Unmarshal(rw.Payload, &req); err != nil {
+			return err
+		}
+		return s.storage.DeleteMessage(&req)
+
+	case "LikeMessage":
+		var req pb.LikeMessageRequest
+		if err := proto.Unmarshal(rw.Payload, &req); err != nil {
+			return err
+		}
+		return s.storage.LikeMessage(&req)
+	// itd.. ...TODO
+
+	default:
+		return fmt.Errorf("unknown op %s", rw.Op)
+	}
 }
-
 
 // ForwardWrite vzpostavi povezavo z succ in mu pošlje rw
 func (s *DataNodeServer) ForwardWrite(rw *pb.ReplicatedWrite) error {
-    succ := s.state.Successor()
-    if succ == nil {
-        return nil // tail, nothing to forward
-    }   
+	succ := s.state.Successor()
+	if succ == nil {
+		return nil // tail, nothing to forward
+	}
 
-    conn, err := grpc.Dial(succ.Address, grpc.WithInsecure())
-    if err != nil {
-        return err 
-    }   
-    defer conn.Close()
+	conn, err := grpc.Dial(succ.Address, grpc.WithInsecure())
+	if err != nil {
+		return err
+	}
+	defer conn.Close()
 
-    client := pb.NewDataNodeClient(conn)
+	client := pb.NewDataNodeClient(conn)
 
-    _, err = client.ReplicateWrite(context.Background(), rw) 
+	_, err = client.ReplicateWrite(context.Background(), rw)
 
-    return err 
+	return err
 }
 
 // sendAckBackward vzpostavi povezavo z pred in mu pošlje ack
 func (s *DataNodeServer) sendAckBackward(version uint64) error {
-    pred := s.state.Predecessor()
-    
-    // povezava s predhodnikom
-    conn, err := grpc.Dial(pred.Address, grpc.WithInsecure())
-    if err != nil {
-        return err 
-    }   
-    defer conn.Close()
+	pred := s.state.Predecessor()
 
-    client := pb.NewDataNodeClient(conn)
+	// povezava s predhodnikom
+	conn, err := grpc.Dial(pred.Address, grpc.WithInsecure())
+	if err != nil {
+		return err
+	}
+	defer conn.Close()
 
-    // repliciraj ack do predhodnika
-    ack := &pb.ReplicatedAck{Version: version}
-    _, err = client.ReplicateAck(context.Background(), ack)
+	client := pb.NewDataNodeClient(conn)
 
-    return err 
+	// repliciraj ack do predhodnika
+	ack := &pb.ReplicatedAck{Version: version}
+	_, err = client.ReplicateAck(context.Background(), ack)
+
+	return err
 }
 
-// ReplicateWrite je grpc metoda (glej proto), ki pošlje rw nasledniku 
-func (s *DataNodeServer) ReplicateWrite(ctx context.Context,req *pb.ReplicatedWrite,) (*emptypb.Empty, error) {
-    // 1. apliciraj lokalno
-    if err := s.applyWrite(rw); err != nil {
-        return nil, err
-    }
-    
-    log.Printf("Prejel sporočilo od predhodnika in ga zapisal v lokalni storage.\n")
-    if s.state.IsTail() {
-        // Rep vrne ACK
-        log.Printf("Kot rep poslal ACK predhodniku.")
-        s.sendAckBackward(req.Version)
-        return &emptypb.Empty{}, nil
-    }
+// ReplicateWrite je grpc metoda (glej proto), ki pošlje rw nasledniku
+func (s *DataNodeServer) ReplicateWrite(ctx context.Context, req *pb.ReplicatedWrite) (*emptypb.Empty, error) {
+	// 1. apliciraj lokalno
+	if err := s.applyWrite(req); err != nil {
+		return nil, err
+	}
 
-    log.Printf("Poslal sporočilo naprej nasledniku.\n")
-    // 2. Pošlji naslednjiku
-    s.ForwardWrite(req)
-    return &emptypb.Empty{}, nil
+	log.Printf("Prejel sporočilo od predhodnika in ga zapisal v lokalni storage.\n")
+	if s.state.IsTail() {
+		// Rep vrne ACK
+		log.Printf("Kot rep poslal ACK predhodniku.")
+		s.sendAckBackward(req.Version)
+		return &emptypb.Empty{}, nil
+	}
+
+	log.Printf("Poslal sporočilo naprej nasledniku.\n")
+	// 2. Pošlji naslednjiku
+	s.ForwardWrite(req)
+	return &emptypb.Empty{}, nil
 }
 
 // ReplicateAck je grpc metoda (glej proto), ki pošlje ra predhodniku
-func (s *DataNodeServer) ReplicateAck(ctx context.Context, req *pb.ReplicatedAck,) (*emptypb.Empty, error) {
-    if s.state.IsHead() {
-        // ACK je prišel do glave
-        // Sporoči čakajočemu klientu (TODO)
-        log.Printf("Kot glava sprejel zadnji ACK.\n")
-        return &emptypb.Empty{}, nil
-    }
-    log.Printf("Poslal ACK predhodniku.\n")
-    // Forward ack backward
-    s.sendAckBackward(req.Version)
-    return &emptypb.Empty{}, nil
+func (s *DataNodeServer) ReplicateAck(ctx context.Context, req *pb.ReplicatedAck) (*emptypb.Empty, error) {
+	if s.state.IsHead() {
+		// ACK je prišel do glave
+		s.mu.Lock()
+		channel, ok := s.pending[req.Version]
+		if ok {
+			close(channel)
+			delete(s.pending, req.Version)
+		}
+		s.mu.Unlock()
+		log.Printf("Kot glava sprejel zadnji ACK.\n")
+		return &emptypb.Empty{}, nil
+	}
+	log.Printf("Poslal ACK predhodniku.\n")
+	// Forward ack backward
+	s.sendAckBackward(req.Version)
+	return &emptypb.Empty{}, nil
 }
-
-
-
-
-
-
-
--- internal/server/server.go
+++ internal/server/server.go
@@ -1,195 +1,262 @@
 package server
 
 import (
-    "fmt"
-    "context"
-    "google.golang.org/grpc/codes"
-    "google.golang.org/grpc/status"
-    "google.golang.org/protobuf/proto"
-    "google.golang.org/protobuf/types/known/emptypb"
-    "google.golang.org/protobuf/types/known/timestamppb"
-    pb "github.com/ab76015/razpravljalnica/api/pb"
-    "github.com/ab76015/razpravljalnica/internal/storage"
-    "github.com/ab76015/razpravljalnica/internal/replication"
+	"context"
+	"fmt"
+
+	pb "github.com/ab76015/razpravljalnica/api/pb"
+	"github.com/ab76015/razpravljalnica/internal/replication"
+	"github.com/ab76015/razpravljalnica/internal/storage"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
+	"google.golang.org/protobuf/proto"
+	"google.golang.org/protobuf/types/known/emptypb"
+	"google.golang.org/protobuf/types/known/timestamppb"
 )
 
 type Server struct {
-    pb.UnimplementedMessageBoardServer
-    storage storage.Storage
-    replication *replication.DataNodeServer
+	pb.UnimplementedMessageBoardServer
+	storage     storage.Storage
+	replication *replication.DataNodeServer
 }
 
 func NewMessageBoardServer(s storage.Storage, r *replication.DataNodeServer) *Server {
-    return &Server{storage: s, replication: r,}
+	return &Server{storage: s, replication: r}
 }
 
 // Pravilo: Za pisalne metode se najprej replicira nato zapiše v storage! (Replication pipeline aplicira write tj. replication/node.go)
 
 // CreateUser je pisalna metoda, ki ustvari novega uporabnika
 func (s *Server) CreateUser(ctx context.Context, in *pb.CreateUserRequest) (*pb.User, error) {
-    // v ns shrani nodestate oz. stanje ki ga dobiš preko getterja State(), 
-    // ta je definiran v replication/node.go za DataNodeServer
-    ns := s.replication.State()
-    if !ns.IsHead() {
-        fmt.Println("server.go: CreateUser() error: writes are only allowed on head node!")
-        return nil, status.Errorf(codes.FailedPrecondition, "writes allowed only on head node")
-    }
-    user, err := s.storage.CreateUser(in.Name)
-    if err != nil {
-        return nil, err
-    }
-    // Zgradi sporočilo za replikacijo (glej proto replicatedwrite)
-    rw := &pb.ReplicatedWrite{
-        Version: ns.Version(),
-        Payload: encodedRequest,
-    }
+	// v ns shrani nodestate oz. stanje ki ga dobiš preko getterja State(),
+	// ta je definiran v replication/node.go za DataNodeServer
+	ns := s.replication.State()
+	if !ns.IsHead() {
+		fmt.Println("server.go: CreateUser() error: writes are only allowed on head node!")
+		return nil, status.Errorf(codes.FailedPrecondition, "writes allowed only on head node")
+	}
 
-    // Repliciraj/pošlji nasledniku (v verigi)
-    s.replication.ForwardWrite(rw)
-    return &pb.User{Id: user.ID, Name: user.Name}, nil
+	data, err := proto.Marshal(in)
+	if err != nil {
+		return nil, err
+	}
+
+	// Zgradi sporočilo za replikacijo (glej proto replicatedwrite)
+	rw := &pb.ReplicatedWrite{
+		Version: ns.Version(),
+		Op:      "CreateUser",
+		Payload: data,
+	}
+
+	// Repliciraj/pošlji nasledniku (v verigi)
+	if err := s.replication.ForwardWrite(rw); err != nil {
+		return nil, err
+	}
+
+	// user, err := s.storage.CreateTopic(in.Name)
+	// if err != nil {
+	// 	return nil, err
+	// }
+
+	//zaenkrat vrnemo optimistečen odgovor, ampak treba implementira wait za ack
+	// return &pb.User{Id: user.ID, Name: user.Name}, nil
+	return &pb.User{}, nil
 }
 
 // CreateTopic je pisalna metoda, ki ustvari novo temo
 func (s *Server) CreateTopic(ctx context.Context, in *pb.CreateTopicRequest) (*pb.Topic, error) {
-    ns := s.replication.State()
-    if !ns.IsHead() {
-        fmt.Println("server.go: CreateTopic() error: writes are only allowed on head node!")
-        return nil, status.Errorf(codes.FailedPrecondition, "writes allowed only on head node")
-    } 
-    topic, err := s.storage.CreateTopic(in.Name)
-    if err != nil {
-        return nil, err
-    }
-    // Zgradi sporočilo za replikacijo (glej proto replicatedwrite)
-    rw := &pb.ReplicatedWrite{
-        Version: ns.Version(),
-        Payload: encodedRequest,
-    }   
+	ns := s.replication.State()
+	if !ns.IsHead() {
+		fmt.Println("server.go: CreateTopic() error: writes are only allowed on head node!")
+		return nil, status.Errorf(codes.FailedPrecondition, "writes allowed only on head node")
+	}
 
-    // Repliciraj/pošlji nasledniku (v verigi)
-    s.replication.ForwardWrite(rw)
-    return &pb.Topic{Id: topic.ID, Name: topic.Name }, nil
+	data, err := proto.Marshal(in)
+	if err != nil {
+		return nil, err
+	}
+
+	// Zgradi sporočilo za replikacijo (glej proto replicatedwrite)
+	rw := &pb.ReplicatedWrite{
+		Version: ns.Version(),
+		Op:      "CreateTopic",
+		Payload: data,
+	}
+
+	// Repliciraj/pošlji nasledniku (v verigi)
+	if err := s.replication.ForwardWrite(rw); err != nil {
+		return nil, err
+	}
+
+	// topic, err := s.storage.CreateTopic(in.Name)
+	// if err != nil {
+	// 	return nil, err
+	// }
+
+	// return &pb.Topic{Id: topic.ID, Name: topic.Name}, nil
+	//zaenkrat vrnemo optimistečen odgovor, ampak treba implementira wait za ack
+	return &pb.Topic{}, nil
 }
 
 // PostMessage je pisalna metoda, ki ustvari novo sporočilo
 func (s *Server) PostMessage(ctx context.Context, in *pb.PostMessageRequest) (*pb.Message, error) {
-    ns := s.replication.State()
-    if !ns.IsHead() {
-        fmt.Println("server.go: PostMessage() error: writes are only allowed on head node!")
-        return nil, status.Errorf(codes.FailedPrecondition, "writes allowed only on head node")
-    } 
+	ns := s.replication.State()
+	if !ns.IsHead() {
+		fmt.Println("server.go: PostMessage() error: writes are only allowed on head node!")
+		return nil, status.Errorf(codes.FailedPrecondition, "writes allowed only on head node")
+	}
 
-    data, err := proto.Marshal(in)
-    if err != nil {
-        return nil, err
-    }
+	data, err := proto.Marshal(in)
+	if err != nil {
+		return nil, err
+	}
 
-    // Zgradi sporočilo za replikacijo (glej proto replicatedwrite)
-    rw := &pb.ReplicatedWrite{
-        Version: ns.Version(),
-        Op: "PostMessage",
-        Payload: data,
-    }
+	// Zgradi sporočilo za replikacijo (glej proto replicatedwrite)
+	rw := &pb.ReplicatedWrite{
+		Version: ns.Version(),
+		Op:      "PostMessage",
+		Payload: data,
+	}
 
+	// Repliciraj/pošlji nasledniku (v verigi)
+	if err := s.replication.ForwardWrite(rw); err != nil {
+		return nil, err
+	}
 
-    // Repliciraj/pošlji nasledniku (v verigi)
-    if err := s.replication.ForwardWrite(rw); err != nil {
-        return nil, err
-    }
-        
-    //return &pb.Message{Id : message.ID, TopicId: message.TopicID, UserId: message.UserID, Text: message.Text, CreatedAt: timestamppb.New(message.CreatedAt), Likes: message.Likes}, nil
-    //zaenkrat vrnemo optimistečen odgovor, ampak treba implementira wait za ack
-    return &pb.Message{}, nil
+	// message, err := s.storage.PostMessage(in.TopicId, in.UserId, in.Text)
+	// if err != nil {
+	// 	return nil, err
+	// }
+
+	//return &pb.Message{Id : message.ID, TopicId: message.TopicID, UserId: message.UserID, Text: message.Text, CreatedAt: timestamppb.New(message.CreatedAt), Likes: message.Likes}, nil
+	//zaenkrat vrnemo optimistečen odgovor, ampak treba implementira wait za ack
+	return &pb.Message{}, nil
 }
 
 // UpdateMessage je pisalna metoda, ki posodobi obstoječe sporočilo
 func (s *Server) UpdateMessage(ctx context.Context, in *pb.UpdateMessageRequest) (*pb.Message, error) {
-    ns := s.replication.State()
-    if !ns.IsHead() {
-        fmt.Println("server.go: UpdateMessage() error: writes are only allowed on head node!")
-        return nil, status.Errorf(codes.FailedPrecondition, "writes allowed only on head node")
-    } 
-    message, err := s.storage.UpdateMessage(in.TopicId, in.UserId, in.MessageId, in.Text)
-    if err != nil {
-        return nil, err
-    }
-    // Zgradi sporočilo za replikacijo (glej proto replicatedwrite)
-    rw := &pb.ReplicatedWrite{
-        Version: ns.Version(),
-        Payload: encodedRequest,
-    }
+	ns := s.replication.State()
+	if !ns.IsHead() {
+		fmt.Println("server.go: UpdateMessage() error: writes are only allowed on head node!")
+		return nil, status.Errorf(codes.FailedPrecondition, "writes allowed only on head node")
+	}
 
-    // Repliciraj/pošlji nasledniku (v verigi)
-    s.replication.ForwardWrite(rw)
-    return &pb.Message{Id : message.ID, TopicId: message.TopicID, UserId: message.UserID, Text: message.Text, CreatedAt: timestamppb.New(message.CreatedAt), Likes: message.Likes}, nil
+	data, err := proto.Marshal(in)
+	if err != nil {
+		return nil, err
+	}
+
+	// Zgradi sporočilo za replikacijo (glej proto replicatedwrite)
+	rw := &pb.ReplicatedWrite{
+		Version: ns.Version(),
+		Op:      "UpdateMessage",
+		Payload: data,
+	}
+
+	// Repliciraj/pošlji nasledniku (v verigi)
+	if err := s.replication.ForwardWrite(rw); err != nil {
+		return nil, err
+	}
+
+	// message, err := s.storage.UpdateMessage(in.TopicId, in.UserId, in.MessageId, in.Text)
+	// if err != nil {
+	// 	return nil, err
+	// }
+
+	//return &pb.Message{Id : message.ID, TopicId: message.TopicID, UserId: message.UserID, Text: message.Text, CreatedAt: timestamppb.New(message.CreatedAt), Likes: message.Likes}, nil
+	//zaenkrat vrnemo optimistečen odgovor, ampak treba implementira wait za ack
+	return &pb.Message{}, nil
 }
 
 // DeleteMessage je pisalna metoda, ki izbrise obstoječe sporočilo
 func (s *Server) DeleteMessage(ctx context.Context, in *pb.DeleteMessageRequest) (*emptypb.Empty, error) {
-    ns := s.replication.State()
-    if !ns.IsHead() {
-        fmt.Println("server.go: DeleteMessage() error: writes are only allowed on head node!")
-        return nil, status.Errorf(codes.FailedPrecondition, "writes allowed only on head node")
-    } 
-    err := s.storage.DeleteMessage(in.TopicId, in.UserId, in.MessageId)
-    if err != nil {
-        return nil, err
-    }
-    // Zgradi sporočilo za replikacijo (glej proto replicatedwrite)
-    rw := &pb.ReplicatedWrite{
-        Version: ns.Version(),
-        Payload: encodedRequest,
-    }
+	ns := s.replication.State()
+	if !ns.IsHead() {
+		fmt.Println("server.go: DeleteMessage() error: writes are only allowed on head node!")
+		return nil, status.Errorf(codes.FailedPrecondition, "writes allowed only on head node")
+	}
+	data, err := proto.Marshal(in)
+	if err != nil {
+		return nil, err
+	}
 
-    // Repliciraj/pošlji nasledniku (v verigi)
-    s.replication.ForwardWrite(rw)
-    return &emptypb.Empty{}, nil
+	// Zgradi sporočilo za replikacijo (glej proto replicatedwrite)
+	rw := &pb.ReplicatedWrite{
+		Version: ns.Version(),
+		Op:      "DeleteMessage",
+		Payload: data,
+	}
+
+	// Repliciraj/pošlji nasledniku (v verigi)
+	if err := s.replication.ForwardWrite(rw); err != nil {
+		return nil, err
+	}
+
+	// err = s.storage.DeleteMessage(in.TopicId, in.UserId, in.MessageId)
+	// if err != nil {
+	// 	return nil, err
+	// }
+
+	//zaenkrat vrnemo optimistečen odgovor, ampak treba implementira wait za ack
+	return &emptypb.Empty{}, nil
 }
 
 // LikeMessage je pisalna metoda, ki vsečka sporočilo
 func (s *Server) LikeMessage(ctx context.Context, in *pb.LikeMessageRequest) (*pb.Message, error) {
-    ns := s.replication.State()
-    if !ns.IsHead() {
-        fmt.Println("server.go: LikeMessage() error: writes are only allowed on head node!")
-        return nil, status.Errorf(codes.FailedPrecondition, "writes allowed only on head node")
-    } 
-    message, err := s.storage.LikeMessage(in.TopicId, in.MessageId, in.UserId)
-    if err != nil {
-        return nil, err
-    }
-    // Zgradi sporočilo za replikacijo (glej proto replicatedwrite)
-    rw := &pb.ReplicatedWrite{
-        Version: ns.Version(),
-        Payload: encodedRequest,
-    }
+	ns := s.replication.State()
+	if !ns.IsHead() {
+		fmt.Println("server.go: LikeMessage() error: writes are only allowed on head node!")
+		return nil, status.Errorf(codes.FailedPrecondition, "writes allowed only on head node")
+	}
 
-    // Repliciraj/pošlji nasledniku (v verigi)
-    s.replication.ForwardWrite(rw)
-    return &pb.Message{Id : message.ID, TopicId: message.TopicID, UserId: message.UserID, Text: message.Text, CreatedAt: timestamppb.New(message.CreatedAt), Likes: message.Likes}, nil
+	data, err := proto.Marshal(in)
+	if err != nil {
+		return nil, err
+	}
+
+	// Zgradi sporočilo za replikacijo (glej proto replicatedwrite)
+	rw := &pb.ReplicatedWrite{
+		Version: ns.Version(),
+		Op:      "LikeMessage",
+		Payload: data,
+	}
+
+	// Repliciraj/pošlji nasledniku (v verigi)
+	if err := s.replication.ForwardWrite(rw); err != nil {
+		return nil, err
+	}
+
+	// message, err := s.storage.LikeMessage(in.TopicId, in.MessageId, in.UserId)
+	// if err != nil {
+	// 	return nil, err
+	// }
+
+	//return &pb.Message{Id : message.ID, TopicId: message.TopicID, UserId: message.UserID, Text: message.Text, CreatedAt: timestamppb.New(message.CreatedAt), Likes: message.Likes}, nil
+	//zaenkrat vrnemo optimistečen odgovor, ampak treba implementira wait za ack
+	return &pb.Message{}, nil
 }
 
-func (s *Server) ListTopics(ctx context.Context,_ *emptypb.Empty) (*pb.ListTopicsResponse, error) {
-    topics, err := s.storage.ListTopics()
-    if err != nil {
-        return nil, err
-    }
-    response := &pb.ListTopicsResponse{}
-    for _, topic := range topics {
-        response.Topics = append(response.Topics, &pb.Topic{Id: topic.ID, Name: topic.Name })
-    }
-    return response, nil
+func (s *Server) ListTopics(ctx context.Context, _ *emptypb.Empty) (*pb.ListTopicsResponse, error) {
+	topics, err := s.storage.ListTopics()
+	if err != nil {
+		return nil, err
+	}
+	response := &pb.ListTopicsResponse{}
+	for _, topic := range topics {
+		response.Topics = append(response.Topics, &pb.Topic{Id: topic.ID, Name: topic.Name})
+	}
+	return response, nil
 }
 
-func (s *Server) GetMessages(ctx context.Context,in *pb.GetMessagesRequest) (*pb.GetMessagesResponse, error) {
-    msgs, err := s.storage.GetMessages(in.TopicId, in.FromMessageId, in.Limit)
-    if err != nil {
-        return nil, err
-    }
+func (s *Server) GetMessages(ctx context.Context, in *pb.GetMessagesRequest) (*pb.GetMessagesResponse, error) {
+	msgs, err := s.storage.GetMessages(in.TopicId, in.FromMessageId, in.Limit)
+	if err != nil {
+		return nil, err
+	}
 
-    response := &pb.GetMessagesResponse{}
-    for _, m := range msgs {
-        response.Messages = append(response.Messages, &pb.Message{Id: m.ID, TopicId: m.TopicID, UserId: m.UserID, Text: m.Text, CreatedAt: timestamppb.New(m.CreatedAt), Likes: m.Likes, })
-    }
-    return response, nil
+	response := &pb.GetMessagesResponse{}
+	for _, m := range msgs {
+		response.Messages = append(response.Messages, &pb.Message{Id: m.ID, TopicId: m.TopicID, UserId: m.UserID, Text: m.Text, CreatedAt: timestamppb.New(m.CreatedAt), Likes: m.Likes})
+	}
+	return response, nil
 }
-
